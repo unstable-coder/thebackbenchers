445bfc2ce38dae332497b146381b41259f1e375d
Rohit Raj <rohitsp927@gmail.com>
2024-06-01 00:27:53 +0530

    updated version

--- frontend-v1/vite.config.js
+++ frontend-v1/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.avif', '**/*.webp']
 })
